40669e7f59deb488aac60cd90267d14817ef3028
Moreno <moren.glenardi@gmail.com>
2025-02-14 15:08:26 +0700

    add vercel.json

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -16,6 +16,7 @@ import LayoutAdmin from "../components/layouts/LayoutAdmin";
 export default function Router() {
   return (
     <Routes>
+       <Route path="*" element={<h1> 404 </h1>} />
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginUser />} />
       <Route path="/login/admin" element={<LoginAdmin />} />
